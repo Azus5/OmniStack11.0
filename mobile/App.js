914bf730950918c99f8eb206ccf47f59a057ea05
Abner <abnerchoba@hotmail.com>
2020-03-27 18:15:47 -0300

    Mobile Finalizado

---
+++ mobile/App.js
@@ -0,0 +1,17 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import React from 'react';
+
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
+
+
+//Por padrão todos os componentes do react-native possuem display: flex
+//No react-native, não exite a herança de estilização...
+
